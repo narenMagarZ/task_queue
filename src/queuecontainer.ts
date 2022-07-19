a0fa839fbff784847daa1ee487b8e4054296220e
narenMagarZ <narenmagarz98@gmail.com>
2022-07-20 01:30:31 +0545

    addding pop and push in queuecontainer

---
+++ src/queuecontainer.ts
@@ -0,0 +1,25 @@
+import IORedis from "ioredis";
+class QueueContainer {
+    private redisConn : IORedis
+    private queue : string
+    constructor(ioRedis:IORedis,queue:string){
+        this.redisConn = ioRedis
+        this.queue = queue
+    }
+    async push(task:string,data?:any){
+        
+        let taskInfo = {
+            'name' : task,
+            'data' : typeof data === 'object' ? JSON.stringify(data) : data
+        }
+        let plainTaskInfo = JSON.stringify(taskInfo)
+        await this.redisConn.rpush(this.queue,plainTaskInfo)
+    }
+
+    async pop(){
+        const defaultTimeOut = 2000
+        return await this.redisConn.blpop(this.queue,defaultTimeOut)
+    }
+}
+
+export default QueueContainer
