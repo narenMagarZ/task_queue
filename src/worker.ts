0bf0f556cb65ae1b8478cf21636c226a1cdf7d81
narenMagarZ <narenmagarz98@gmail.com>
2022-07-20 01:31:01 +0545

    listentask to queue

--- src/worker.ts
+++ src/worker.ts
@@ -2,28 +2,33 @@ import IORedis from 'ioredis'
 import EventEmitter from 'node:events'
 import { connector } from './connector'
 import TaskQueue from './task_queue'
+import QueueContainer from './queuecontainer'
 interface connectionOptions {
     connection? : IORedis 
     attempt? : number | 3 | null
 }
 class Worker extends TaskQueue {
     private queue : string
-    private ioRedis : IORedis | null
+    private ioRedis : IORedis
     private listener : EventEmitter
     constructor(queue:string,connection?:IORedis | null){
         super()
         this.queue = queue
-        this.ioRedis = connection ?? null
+        this.ioRedis = connection as IORedis
         this.listener = connector.wire as EventEmitter
     }
-    async listen(name:string,cb:(err:Error | null,data:any)=>void){
-        console.log(this.listener,'from worker')
-        this.listener.on(name,(data)=>{
-            cb(null,data)
+    listen(task:string,cb:(err:Error | null,data:any)=>void){
+        this.listener.on(task,async()=>{
+            const queueContainer = new QueueContainer(this.ioRedis,this.queue)
+            const poppedTask = await queueContainer.pop()
+            console.log(poppedTask,'this is popped data')
+            if(poppedTask){
+                cb(null,poppedTask)
+            }
         })
     }
     
-    private executeTask(){
+    private executeTask(cb:(err:Error|null,data:any)=>void){
 
     }
 
