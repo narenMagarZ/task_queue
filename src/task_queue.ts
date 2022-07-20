c8a77b99c334d8ef31257b4dcc6b54d83a7c1ca3
narenMagarZ <narenmagarz98@gmail.com>
2022-07-20 08:31:25 +0545

    nothing

--- src/task_queue.ts
+++ src/task_queue.ts
@@ -1,9 +1,7 @@
 import EventEmitter from 'node:events'
 abstract class TaskQueue extends EventEmitter{
-    queueName : string 
     constructor(){
         super()
-        this.queueName = ''
     }
     protected pipeline(task:string,data:any){
         this.emit(task,data)
