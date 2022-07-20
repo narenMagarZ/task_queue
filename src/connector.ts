de883bdfc5bbb50a1b9926d1d5683c93ad4e8ab8
narenMagarZ <narenmagarz98@gmail.com>
2022-07-20 08:27:50 +0545

    added queueidentifier

--- src/connector.ts
+++ src/connector.ts
@@ -1,10 +1,14 @@
 import EventEmitter from "node:events"
 import Queue from "./queue"
-class Connector extends EventEmitter {
+import TaskQueue from "./task_queue"
+import {v4 as uuidv4} from 'uuid'
+class Connector extends TaskQueue {
     public wire : EventEmitter | null
     private queue : Queue | null
+    public queueIdentifier : string
     constructor(){
         super()
+        this.queueIdentifier = uuidv4().split('-').join('')
         this.wire = new EventEmitter()
         this.queue = null
     }
